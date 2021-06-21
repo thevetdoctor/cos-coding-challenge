5f4c1db21dd316d200d8bfc2c05d2ffabc389797
obafemi oderanti <thevetdoctor@gmail.com>
2021-06-21 03:40:09 +0100

    feature(interface): implement ICarOnSaleClient interface

--- challenges/backend/src/app/services/CarOnSaleClient/interface/ICarOnSaleClient.ts
+++ challenges/backend/src/app/services/CarOnSaleClient/interface/ICarOnSaleClient.ts
@@ -3,6 +3,38 @@
  */
 export interface ICarOnSaleClient {
 
-    getRunningAuctions(): Promise<any /* TODO: Introduce a type */>
+    /* TODO: Introduce a type */
+    getRunningAuctions(): Promise<IAuction>[] 
 
+}
+
+export interface IAuction {
+        label: string,
+        associatedVehicle : IAssociatedVehicle
+        endingTime: string,
+        remainingTimeInSeconds: number,
+        currentHighestBidValue: number
+}
+
+export interface IAssociatedVehicle {
+        make: string,
+        model: string,
+        vehicleImages: IVehicleImage[],
+        mileageInKm: number,
+        category: number,
+        fuelType: number,
+        transmission: number,
+        ez: string
+}
+
+export interface IVehicleImage {
+        uuid: string,
+        rawData: null,
+        url: string,
+        _fk_uuid_vehicle: string,
+        perspective: number,
+        encoding: null,
+        mimeType: null,
+        createdAt: string,
+        updatedAt: string
 }
